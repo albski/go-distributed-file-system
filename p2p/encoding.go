cde55f9ad658e5075072c2045e389c801b7497f9
Albert Skonieczny <albertskonieczny@gmail.com>
2024-06-09 11:37:51 +0200

    Refactor Message to RPC

--- p2p/encoding.go
+++ p2p/encoding.go
@@ -6,25 +6,25 @@ import (
 )
 
 type Decoder interface {
-	Decode(io.Reader, *Message) error
+	Decode(io.Reader, *RPC) error
 }
 
 type GOBDecoder struct{}
 
-func (d GOBDecoder) Decode(r io.Reader, msg *Message) error {
-	return gob.NewDecoder(r).Decode(msg)
+func (d GOBDecoder) Decode(r io.Reader, rpc *RPC) error {
+	return gob.NewDecoder(r).Decode(rpc)
 }
 
 type DefaultDecoder struct{}
 
-func (d DefaultDecoder) Decode(r io.Reader, msg *Message) error {
+func (d DefaultDecoder) Decode(r io.Reader, rpc *RPC) error {
 	buf := make([]byte, 1024)
 	n, err := r.Read(buf)
 	if err != nil {
 		return err // temp
 	}
 
-	msg.Payload = buf[:n]
+	rpc.Payload = buf[:n]
 
 	return nil
 }
--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -73,13 +73,15 @@ func (t *TCPTransport) handleConn(conn net.Conn) {
 		return
 	}
 
-	msg := &Message{}
+	rpc := &RPC{}
 	for {
-		if err := t.Decoder.Decode(conn, msg); err != nil {
+		if err := t.Decoder.Decode(conn, rpc); err != nil {
 			fmt.Printf("TCP error: %s", err)
 			continue
 		}
 
-		fmt.Printf("message: %v", msg)
+		rpc.From = conn.RemoteAddr()
+
+		fmt.Printf("message: %v", rpc)
 	}
 }
