e7c5fe4aa1ed5808c97b0c45f6ac10eef914f70c
Albert Skonieczny <albertskonieczny@gmail.com>
2024-06-10 14:46:37 +0200

    Refactor path managment

---
+++ path.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"crypto/sha1"
+	"encoding/hex"
+	"path/filepath"
+	"strings"
+)
+
+const defaultRootDir = "network" // make it env var later
+
+type KeyPath struct {
+	Key  string
+	Path string // Path is based on Key
+}
+
+func (k KeyPath) dirPath(basePath string) string {
+	return k.joinPaths(basePath, k.Path)
+}
+
+func (k KeyPath) fullPath(basePath string) string {
+	return k.joinPaths(basePath, k.Path, k.Key)
+}
+
+func (k KeyPath) rootPath(basePath string) string {
+	fullPath := k.joinPaths("", k.Path, k.Key)
+	rootPath := strings.Split(filepath.ToSlash(fullPath), "/")[0]
+	return k.joinPaths(basePath, rootPath)
+}
+
+func (k KeyPath) joinPaths(basePath string, paths ...string) string {
+	if basePath == "" {
+		return filepath.Join(paths...)
+	}
+	basePath = filepath.Clean(basePath)
+	return filepath.Join(append([]string{basePath}, paths...)...)
+}
+
+type transformPathFunc func(string) KeyPath
+
+func transformPathCrypt(key string) KeyPath {
+	hash := sha1.Sum([]byte(key))
+	hashStr := hex.EncodeToString(hash[:])
+
+	blockSize := 5
+	sliceLen := len(hashStr) / blockSize
+	paths := make([]string, sliceLen)
+
+	for i := 0; i < sliceLen; i++ {
+		from, to := i*blockSize, (i*blockSize)+blockSize
+		paths[i] = hashStr[from:to]
+	}
+
+	return KeyPath{
+		Key:  hashStr,
+		Path: filepath.Join(paths...),
+	}
+}
--- storage.go
+++ storage.go
@@ -2,16 +2,10 @@ package main
 
 import (
 	"bytes"
-	"crypto/sha1"
-	"encoding/hex"
 	"io"
 	"log"
 	"os"
-	"path/filepath"
-	"strings"
 )
-
-const defaultRootDir = "network"
 
 type StorageOpts struct {
 	rootDir           string
@@ -28,59 +22,6 @@ func NewStorage(opts StorageOpts) *Storage {
 	}
 
 	return &Storage{StorageOpts: opts}
-}
-
-type KeyPath struct {
-	Key  string
-	Path string // Path is based on Key
-}
-
-func (k KeyPath) dirPath(basePath string) string {
-	if basePath == "" {
-		return k.Path
-	}
-	basePath = filepath.Clean(basePath)
-	return filepath.Join(basePath, k.Path)
-}
-
-func (k KeyPath) fullPath(basePath string) string {
-	if basePath == "" {
-		return filepath.Join(k.Path, k.Key)
-	}
-
-	basePath = filepath.Clean(basePath)
-	return filepath.Join(basePath, k.Path, k.Key)
-}
-
-func (k KeyPath) rootPath(basePath string) string {
-	if basePath == "" {
-		return filepath.Join(k.Path, k.Key)
-	}
-
-	basePath = filepath.Clean(basePath)
-	rootPath := strings.Split(filepath.ToSlash(k.fullPath("")), "/")[0]
-	return filepath.Join(basePath, rootPath)
-}
-
-type transformPathFunc func(string) KeyPath
-
-func transformPathCrypt(key string) KeyPath {
-	hash := sha1.Sum([]byte(key))
-	hashStr := hex.EncodeToString(hash[:])
-
-	blockSize := 5
-	sliceLen := len(hashStr) / blockSize
-	paths := make([]string, sliceLen)
-
-	for i := 0; i < sliceLen; i++ {
-		from, to := i*blockSize, (i*blockSize)+blockSize
-		paths[i] = hashStr[from:to]
-	}
-
-	return KeyPath{
-		Key:  hashStr,
-		Path: filepath.Join(paths...),
-	}
 }
 
 func (s *Storage) Delete(key string) error {
